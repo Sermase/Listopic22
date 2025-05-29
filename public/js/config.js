b66d11f77fdb814d98deca73e42274dbedb15e08
Sermase <sergio.marserr@gmail.com>
2025-05-29 14:26:57 +0200

    Update config.js

--- public/js/config.js
+++ public/js/config.js
@@ -2,7 +2,7 @@ window.ListopicApp = window.ListopicApp || {};
 
 ListopicApp.config = {
     firebaseConfig: {
-        apiKey: "AIzaSyA92bOm3sHQh_j9b9WLQAlvxKGmXKc99gk", // Reemplaza con tu API Key real
+        apiKey: "AIzaSyDPEW5zXtvfnD0XtdmXSkMBZrsFdO-tmsg", // Reemplaza con tu API Key real
         authDomain: "listopic.firebaseapp.com",
         databaseURL: "https://listopic-default-rtdb.europe-west1.firebasedatabase.app",
         projectId: "listopic",
