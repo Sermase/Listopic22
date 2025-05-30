1ec21878fcf8d64497ea0822e6ad7cc9e7b1a818
Sermase <sergio.marserr@gmail.com>
2025-05-30 09:56:00 +0200

    Update authService.js

--- public/js/authService.js
+++ public/js/authService.js
@@ -4,7 +4,7 @@ ListopicApp.authService = (() => {
     // Dependencies:
     // ListopicApp.services.auth (Firebase auth instance)
     // DOM elements: 'userProfileBtn', 'userMenuDropdown', 'logoutBtnUserMenu', 'userInfoDisplay', 'logout-button'
-
+/*
     function init() {
         if (!ListopicApp.services || !ListopicApp.services.auth) {
             console.error("authService.init: Firebase auth service not available. Ensure firebaseService.js is loaded and initialized before authService.js");
@@ -114,3 +114,5 @@ ListopicApp.authService = (() => {
         init
     };
 })();
+
+*/
