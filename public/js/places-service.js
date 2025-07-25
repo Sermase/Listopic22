eb37d7af98ba76dadb00fe7e7fcde72c197a1e9e
Sermase <sergio.marserr@gmail.com>
2025-07-25 03:38:14 +0200

    Add admin page and update Firestore rules and UI
    Introduces a new admin page (admin.html) and its JavaScript (page-admin.js). Updates Firestore security rules, emulator configuration, and various UI files to support admin features and improve integration. Also adds Firestore emulator debug log for troubleshooting.

--- public/js/places-service.js
+++ public/js/places-service.js
@@ -23,6 +23,7 @@ ListopicApp.placesService = (() => {
                             longitude: place.geometry?.location?.lng || null,
 <<<<<<< Updated upstream:public/js/places-service.js
 <<<<<<< Updated upstream:public/js/places-service.js
+<<<<<<< Updated upstream:public/js/places-service.js
                             // mapsUrl: place.place_id 
                             //     ? `https://www.google.com/maps/search/?api=1&query=${encodeURIComponent(place.name)}&query_place_id=${place.place_id}` 
                             //     : (place.geometry?.location?.lat && place.geometry?.location?.lng 
@@ -32,6 +33,8 @@ ListopicApp.placesService = (() => {
                             googleMapsUrl: place.place_id ? `https://www.google.com/maps/search/?api=1&query_place_id=${place.place_id}` : (place.geometry?.location?.lat ? `https://www.google.com/maps/search/?api=1&query=${place.geometry.location.lat},${place.geometry.location.lng}` : ''),
 
 =======
+=======
+>>>>>>> Stashed changes:public/js/placesService.js
 =======
 >>>>>>> Stashed changes:public/js/placesService.js
                             // --- CORRECCIÓN CLAVE AQUÍ ---
