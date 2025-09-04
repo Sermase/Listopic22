0620117dbeaf40f0dfb183932726487ba3c75df2
Sermase <sergio.marserr@gmail.com>
2025-09-04 15:09:31 +0200

    Actualizar config.js

--- public/js/config.js
+++ public/js/config.js
@@ -43,7 +43,8 @@ ListopicApp.config = {
         groupedReviews: "https://groupedreviews-jz4x2l2cfq-ew.a.run.app",
         placesNearbyRestaurants: "https://placesnearbyrestaurants-jz4x2l2cfq-ew.a.run.app",
         placesTextSearch: "https://placestextsearch-jz4x2l2cfq-ew.a.run.app",
-        getPlaceDetails: "https://getplacedetails-jz4x2l2cfq-ew.a.run.app"
+        getPlaceDetails: "https://getplacedetails-jz4x2l2cfq-ew.a.run.app", 
+reverseGeocode: "https://europe-west1-listopic.cloudfunctions.net/reverseGeocode" 
     },
     // Añade aquí tus claves de Algolia cuando las tengas en un lugar seguro
     ALGOLIA_APP_ID: 'FI4Q0XQABV',
